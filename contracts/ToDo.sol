410bd1d7f94f294fbdf6043dfddfd51e7bd09cbc
Tochy112 <ukwuomatochi112@gmail.com>
2025-03-16 23:45:25 +0100

    updated

--- contracts/ToDo.sol
+++ contracts/ToDo.sol
@@ -1,50 +1,144 @@
 // SPDX-License-Identifier: UNLICENSED
-
-pragma solidity ^0.8.28;
+pragma solidity ^0.8.2;
 
 contract ToDo {
     address private owner;
+    uint256 private todoCount;
 
-    struct ToDoList{
+    struct ToDoItem {
+        uint256 id;
         string title;
-        string ownerAddress;
+        string description;
+        address ownerAddress;
+        bool completed;
+        uint256 createdAt;
+        uint256 updatedAt;
     }
 
-    ToDoList[] private ToDos;
+    // Mapping for better data access - id to ToDoItem
+    mapping(uint256 => ToDoItem) private todos;
+    
+    // Array of all todo IDs
+    uint256[] private todoIds;
 
-    constructor(){
+    // Events 
+    event TodoAdded(uint256 indexed id, string title, address ownerAddress);
+    event TodoUpdated(uint256 indexed id, string newTitle, address ownerAddress);
+    event TodoCompleted(uint256 indexed id);
+    event TodoRemoved(uint256 indexed id);
+   
+
+    constructor() {
         owner = msg.sender;
+        todoCount = 0;
     }
 
-    modifier ownerModifier{
-        require(msg.sender == owner, "You're not authorized");
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not authorized");
         _;
     }
 
-
-    function addToDo(string memory _title, string memory _ownerAddress) public ownerModifier{
-        ToDos.push(ToDoList(_title, _ownerAddress));
+    modifier validTodoId(uint256 _id) {
+        require(_id > 0 && _id <= todoCount && todos[_id].id == _id, "Invalid todo ID");
+        _;
     }
 
-    function fetchToDo() public view returns(ToDoList[] memory ) {
-        return ToDos;
-    }
-
-
-   function updateToDo(uint256 _index, string memory _newTitle, string memory _newOwnerAddress) public ownerModifier{
-        require(_index < ToDos.length, "No todo with such index found");
+   
+    function addToDo(
+        string memory _title, 
+        string memory _description, 
+        address _ownerAddress
+    ) public onlyOwner returns (uint256) {
+        require(bytes(_title).length > 0, "Title cannot be empty");
         
-        ToDos[_index].title = _newTitle;
-        ToDos[_index].ownerAddress = _newOwnerAddress;
+        todoCount++;
+        
+        // Create and store the new todo item
+        todos[todoCount] = ToDoItem({
+            id: todoCount,
+            title: _title,
+            description: _description,
+            ownerAddress: _ownerAddress,
+            completed: false,
+            createdAt: block.timestamp,
+            updatedAt: block.timestamp
+        });
+        
+        todoIds.push(todoCount);
+        
+        // Emit event
+        emit TodoAdded(todoCount, _title, _ownerAddress);
+        
+        return todoCount;
     }
 
-    function removeToDo(uint256 _index) public ownerModifier{
-        require(_index < ToDos.length, "No todo which such index found");
-        for (uint256 i = 0; i < ToDos.length - 1; i++) {
-            ToDos[i] =  ToDos[i + 1];
+
+    function fetchAllToDos() public view returns (ToDoItem[] memory) {
+        ToDoItem[] memory allTodos = new ToDoItem[](todoIds.length);
+        
+        for (uint256 i = 0; i < todoIds.length; i++) {
+            allTodos[i] = todos[todoIds[i]];
         }
-        ToDos.pop();
+        
+        return allTodos;
     }
 
+    function fetchToDoById(uint256 _id) public view validTodoId(_id) returns (ToDoItem memory) {
+        return todos[_id];
+    }
 
+    function updateToDo(
+        uint256 _id, 
+        string memory _newTitle, 
+        string memory _newDescription, 
+        address _newOwnerAddress
+    ) public onlyOwner validTodoId(_id) {
+        require(bytes(_newTitle).length > 0, "Title cannot be empty");
+        
+        todos[_id].title = _newTitle;
+        todos[_id].description = _newDescription;
+        todos[_id].ownerAddress = _newOwnerAddress;
+        todos[_id].updatedAt = block.timestamp;
+        
+        emit TodoUpdated(_id, _newTitle, _newOwnerAddress);
+    }
+
+   
+    function completeToDo(uint256 _id) public onlyOwner validTodoId(_id) {
+        require(!todos[_id].completed, "Todo is already completed");
+        
+        todos[_id].completed = true;
+        todos[_id].updatedAt = block.timestamp;
+        
+        emit TodoCompleted(_id);
+    }
+
+   
+    function removeToDo(uint256 _id) public onlyOwner validTodoId(_id) {
+        uint256 indexToRemove;
+        bool found = false;
+        
+        for (uint256 i = 0; i < todoIds.length; i++) {
+            if (todoIds[i] == _id) {
+                indexToRemove = i;
+                found = true;
+                break;
+            }
+        }
+        
+        require(found, "Todo ID not found in the array");
+        
+        if (indexToRemove < todoIds.length - 1) {
+            todoIds[indexToRemove] = todoIds[todoIds.length - 1];
+        }
+        todoIds.pop();
+        
+        delete todos[_id];
+        
+        emit TodoRemoved(_id);
+    }
+
+    function getTodoCount() public view returns (uint256) {
+        return todoIds.length;
+    }
 }
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,12 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: "https://wild-wild-asphalt.ethereum-sepolia.quiknode.pro/4d49c2ad72fe7b0c3fa2334c49dd40121ed1a520/",
+      accounts: [process.env.PRIVATE_KEY!]
+    }
+  }
 };
 
 export default config;
